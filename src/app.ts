bb2284cf2c09fffb6c62153833831f29a8459101
Alvaro <alvar01omer@gmail.com>
2020-11-17 19:50:55 -0300

    codebase complete

--- src/app.ts
+++ src/app.ts
@@ -7,5 +7,5 @@ app.use(bodyParser.json({
         req.rawBody = buf;
     }
 }));
-app.get('/', (req, res) => res.send('Helloasd  World!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 export {app};
